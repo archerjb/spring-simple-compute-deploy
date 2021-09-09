b2013ddebf7e0af6aa9a2828ff1c22bfe60fad73
Josh Archer <jarcher@greenriver.edu>
2021-09-09 15:49:50 -0700

    Adding initial files.

---
+++ src/main/java/com/example/springsimplecomputedeploy/SpringSimpleComputeDeployApplication.java
@@ -0,0 +1,15 @@
+package com.example.springsimplecomputedeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleComputeDeployApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringSimpleComputeDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsimplecomputedeploy/SpringSimpleComputeDeployApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.springsimplecomputedeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleComputeDeployApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
